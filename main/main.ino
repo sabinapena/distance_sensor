459beb3091d0f3416fe3143538fd4115e7f63cdd
salpeha <salpeha@proton.me>
2026-04-22 18:32:16 +0200

    file added

---
+++ main/main.ino
@@ -0,0 +1,28 @@
+#include <Wire.h> // for 12c communication
+#include <Adafruit_VL53L0X.h>
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X(); //instance
+
+void setup(){
+  Serial.begin(115200); // initiate serial communication
+  //Wire.begin();
+  delay(1000);
+
+  if(!lox.begin()){
+    Serial.println("Failed to boot dist sensor");
+    while(1);
+  }
+  Serial.println("Sensor ready\n");
+}
+
+void loop(){
+  VL53L0X_RangingMeasurementData_t measure;
+  lox.rangingTest(&measure);
+  if(measure.RangeStatus != 4){
+    Serial.print("Distance (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+  }else{
+    Serial.println("Object out of range");
+  }
+  delay(100);
+}
